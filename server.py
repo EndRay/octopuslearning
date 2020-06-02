a1372ca8be87418921375099f2520c58137e40e7
Igor Kovrigin <ikovrigin@kmilearning.com>
2020-06-02 10:29:48 +0300

    move virtual rating data to api

--- server.py
+++ server.py
@@ -14,10 +14,16 @@ def graph():
 def root():
     return app.send_static_file('task.html')
 
+
 @app.route('/api/taskPerformance/<contest>/<index>')
 def task_info(contest, index):
     contest_id = int(contest)
     return jsonify(task_performance_stats(contest_id, index))
+
+
+@app.route('/api/virtualRating/<handle>')
+def virtual_rating(handle):
+    return jsonify([{"x": 1488719100, "y": 1485, "delta": -15, "contest": ["Codeforces Round #403 (Div. 2, based on Technocup 2017 Finals)"]}, {"x": 1489590300, "y": 1549, "delta": 64, "contest": ["Codeforces Round #404 (Div. 2)"]}])
 
 
 if __name__ == "__main__":
