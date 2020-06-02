02db2fb8087e20fb820c10b8b22d9c51e84c3495
Igor Kovrigin <ikovrigin@kmilearning.com>
2020-06-02 22:51:40 +0300

    favicon added

--- server.py
+++ server.py
@@ -27,5 +27,9 @@ def get_virtual_rating(handle):
     return jsonify(virtual_rating.calculate(handle))
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
 if __name__ == "__main__":
     app.run(debug=True)
