2a42392f212b5ee5b0594d4200511dcd949b9d5d
Igor Kovrigin <ikovrigin@kmilearning.com>
2020-06-04 15:32:49 +0300

    fix stats

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
 
+from codeforces.analysis.tasks_performance import task_performance_stats
 from codeforces.rating_system import virtual_rating
-from codeforces.analysis import task_performance_stats
 
 app = Flask(__name__)
 
